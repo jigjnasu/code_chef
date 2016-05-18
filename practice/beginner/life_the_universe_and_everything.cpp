f3f8c228b8d8954f4071bcbc8ed9c9cb8dd0b303
cpp.rakesh <cpp.rakesh@gmail.com>
2016-05-18 06:56:06 +0100

    Solution for  Life, the Universe, and Everything Solved

---
+++ practice/beginner/life_the_universe_and_everything.cpp
@@ -0,0 +1,18 @@
+/*
+  Author @ Rakesh Kumar
+  cpp.rakesh@gmail.com
+ */
+
+#include <cstdio>
+
+int main() {
+	while (1) {
+		int value = 0;
+		scanf("%d", &value);
+		if (value == 42)
+			break;
+		printf("%d\n", value);
+	}
+
+	return 0;
+}
