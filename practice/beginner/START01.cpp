b3c638bc866814c405559fc427f7e74a1f0c7627
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-08-25 04:20:30 +1000

    number mirror

---
+++ practice/beginner/START01.cpp
@@ -0,0 +1,19 @@
+/*
+  Problem Code : START01
+  Problem : Number Mirror
+  Author : Rakesh Kumar
+  Date: 25/08/2020
+*/
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    std::string s;
+    std::getline(std::cin, s);
+    std::cout << s << std::endl;
+
+    return 0;
+}
