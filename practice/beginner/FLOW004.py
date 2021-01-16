053f57562e34a9a72e54e3686731faea805a5ddf
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-01-17 10:50:17 +1100

    small factorials

---
+++ practice/beginner/FLOW004.py
@@ -0,0 +1,15 @@
+'''
+  Problem : Small factorials
+  Author @ Rakesh Kumar
+  cpp.rakesh@gmail.com
+  Date : 17/01/2021
+'''
+
+def solve(n):
+    f = ord(n[0]) - ord('0')
+    l = ord(n[len(n) - 1]) - ord('0')
+    print(f + l)
+
+if __name__ == '__main__':
+    for i in range(int(input())):
+        solve(str(input()))
