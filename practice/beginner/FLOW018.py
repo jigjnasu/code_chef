ccbcf419c2db891908e6ad9b7710cd11f9f53457
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-01-26 11:19:24 +1100

    small factorial

---
+++ practice/beginner/FLOW018.py
@@ -0,0 +1,19 @@
+'''
+  Problem : Small Factorial
+  Author @ Rakesh Kumar
+  cpp.rakesh@gmail.com
+  Date : 26/01/2021
+'''
+
+def fac():
+    f = [1, 1]
+    for i in range(2, 22):
+        f.append(f[i - 1] * i)
+    return f
+
+if __name__ == '__main__':
+    f = fac()
+    for i in range(int(input())):
+        n = int(input())
+        print(f[n])
+        
