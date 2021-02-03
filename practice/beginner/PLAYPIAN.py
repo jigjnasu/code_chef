fad2cbed2bcf7c065e4e65f8398126f8947abd45
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-02-04 06:55:06 +1100

    play piano

---
+++ practice/beginner/PLAYPIAN.py
@@ -0,0 +1,19 @@
+'''
+  Problem : Id and Ship
+  Author @ Rakesh Kumar
+  cpp.rakesh@gmail.com
+  Date : 04/02/2021
+'''
+
+def solve():
+    for _ in range(int(input())):
+        s = str(input())
+        r = 'yes'
+        for i in range(0, len(s), 2):
+            if s[i] == s[i + 1]:
+                r = 'no'
+                break
+        print(r)
+
+if __name__ == '__main__':
+    solve()
