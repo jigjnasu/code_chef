4df39d3a5cfcf3b1e218df008cd7ca22541995f5
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-02-08 01:10:30 +1100

    grade the steel

---
+++ practice/beginner/HEADBOB.py
@@ -0,0 +1,20 @@
+'''
+  Problem : Grade The Steel
+  Author @ Rakesh Kumar
+  cpp.rakesh@gmail.com
+  Date : 08/02/2021
+'''
+
+def solve():
+    for _ in range(int(input())):
+        n = int(input())
+        s = str(input())
+        if 'I' in s:
+            print('INDIAN')
+        elif 'Y' in s:
+            print('NOT INDIAN')
+        else:
+            print('NOT SURE')
+
+if __name__ == '__main__':
+    solve()
