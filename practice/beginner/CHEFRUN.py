0b54b872bdc1c46365313698b224c18c39615828
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-02-11 14:13:05 +1100

    secret recipe

---
+++ practice/beginner/CHEFRUN.py
@@ -0,0 +1,21 @@
+'''
+  Problem : Candy Love
+  Author @ Rakesh Kumar
+  cpp.rakesh@gmail.com
+  Date : 11/02/2021
+'''
+
+def solve():
+    for _ in range(int(input())):
+        a = list(map(int, input().split()))
+        t1 = abs(a[2] - a[0]) / a[3]
+        t2 = abs(a[2] - a[1]) / a[4]
+        if t1 > t2:
+            print('Kefa')
+        elif t1 < t2:
+            print('Chef')
+        else:
+            print('Draw')
+
+if __name__ == '__main__':
+    solve()
