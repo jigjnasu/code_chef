3e1a5e9d18dc4c355f509209e0dba3ec9f7342f4
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-02-14 10:13:29 +1100

    coins and triangles

---
+++ practice/beginner/TRICOIN.py
@@ -0,0 +1,20 @@
+'''
+  Problem : Chef and the Wildcard Matching
+  Author @ Rakesh Kumar
+  cpp.rakesh@gmail.com
+  Date : 14/02/2021
+'''
+
+def solve():
+    for _ in range(int(input())):
+        n = int(input())
+        h = 1
+        while True:
+            s = (h * (h + 1)) >> 1
+            if s > n:
+                print(h - 1)
+                break
+            h += 1
+
+if __name__ == '__main__':
+    solve()
