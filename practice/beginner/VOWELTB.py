99f1b20d2dfa8c73d45b5d5606f0bfb35626fe1a
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-02-14 05:39:59 +1100

    vowel

---
+++ practice/beginner/VOWELTB.py
@@ -0,0 +1,15 @@
+'''
+  Problem : Is it a VOWEL or CONSONANT
+  Author @ Rakesh Kumar
+  cpp.rakesh@gmail.com
+  Date : 14/02/2021
+'''
+
+def solve():
+    if str(input()) in ['A', 'E', 'I', 'O', 'U']:
+        print('Vowel')
+    else:
+        print('Consonant')
+
+if __name__ == '__main__':
+    solve()
