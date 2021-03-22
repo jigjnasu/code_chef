f8815a5ae28caef0ce5e7959c295387f883319a9
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-03-22 23:55:24 +1100

    hidden numbers

---
+++ practice/beginner/UWCOI21A.py
@@ -0,0 +1,14 @@
+'''
+  Problem : Hidden Numbers
+  Author @ Rakesh Kumar
+  cpp.rakesh@gmail.com
+  Date : 22/03/2021
+'''
+
+def solve():
+    for _ in range(int(input())):
+        n = int(input())
+        print(1, n)
+
+if __name__ == '__main__':
+    solve()
