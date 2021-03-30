3cccf0200558b08f477f30506c9d9048c270a7c6
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-03-30 22:55:51 +1100

    chef and practice control

---
+++ practice/beginner/CHEFSTEP.py
@@ -0,0 +1,20 @@
+'''
+  Problem : Chef and Price Control
+  Author @ Rakesh Kumar
+  cpp.rakesh@gmail.com
+  Date : 30/03/2021
+'''
+
+def solve():
+    for _ in range(int(input())):
+        n, k = map(int, input().split())
+        a = list(map(int, input().split()))
+        for d in a:
+            if d % k == 0:
+                print('1', end='')
+            else:
+                print('0', end='')
+        print()
+
+if __name__ == '__main__':
+    solve()
